8a850b46c5f58e3ab268f54381ab5ac13d1b0913
Manfred Quasten <manfred.quasten@arcor.de>
2013-04-14 15:37:36 +0200

    CustomerAO domainConverter

--- merchandise.web/src/main/java/de/mq/merchandise/customer/support/CustomerAO2DomainConverter.java
+++ merchandise.web/src/main/java/de/mq/merchandise/customer/support/CustomerAO2DomainConverter.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 import de.mq.merchandise.customer.Customer;
 
 @Component
-public class CustomerAO2DomainConverter implements Converter<CustomerAO, Customer>{
+class CustomerAO2DomainConverter implements Converter<CustomerAO, Customer>{
 
 	@Override
 	public Customer convert(final CustomerAO customerModel) {
