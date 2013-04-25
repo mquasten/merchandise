95b04dd6685249c3fd0811d926f49c38ff90de77
Manfred Quasten <manfred.quasten@arcor.de>
2013-04-25 21:50:59 +0200

    interface for specific authentification provider

--- merchandise.application/merchandise.application/src/main/java/de/mq/merchandise/customer/support/AuthentificationService.java
+++ merchandise.application/merchandise.application/src/main/java/de/mq/merchandise/customer/support/AuthentificationService.java
@@ -2,7 +2,19 @@ package de.mq.merchandise.customer.support;
 
 import org.springframework.security.authentication.AuthenticationProvider;
 
+/**
+ * Specific AuthentificationProvider. Puts user password into security context.
+ * Use this token for  authentification, get roles from database
+ * @author Admin
+ *
+ */
 public interface AuthentificationService  extends AuthenticationProvider {
 
+	/**
+	 * Put a security token into the security context, that can be used to authentificate a person customer combination.
+	 * @param userId the id of the person
+	 * @param customerId the id from the (current) customer
+	 * @param credentials an encrypted password for the customer,
+	 */
 	void createSecurityToken(long userId, long customerId, String credentials);
 }
