d00dcf2b42bc461e1347419d5752d81a03fe759d
Manfred Quasten <manfred.quasten@arcor.de>
2013-09-21 16:32:33 +0200

    unused imports

--- merchandise.application/merchandise.application/src/main/java/de/mq/merchandise/opportunity/support/ResourceOperations.java
+++ merchandise.application/merchandise.application/src/main/java/de/mq/merchandise/opportunity/support/ResourceOperations.java
@@ -3,7 +3,6 @@ package de.mq.merchandise.opportunity.support;
 import java.awt.image.BufferedImage;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.URL;
 
 
 /**
--- merchandise.application/merchandise.application/src/main/java/de/mq/merchandise/opportunity/support/ResourceTemplateImpl.java
+++ merchandise.application/merchandise.application/src/main/java/de/mq/merchandise/opportunity/support/ResourceTemplateImpl.java
@@ -1,7 +1,6 @@
 package de.mq.merchandise.opportunity.support;
 
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
