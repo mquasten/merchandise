dbee1c1854570c409e4b065e93b2f1caee72e2cf
Manfred Quasten <manfred.quasten@arcor.de>
2013-11-19 20:23:08 +0100

    cosmetics

--- merchandise.domain/src/test/java/de/mq/merchandise/opportunity/support/OpportunityIndexIntegrationTest.java
+++ merchandise.domain/src/test/java/de/mq/merchandise/opportunity/support/OpportunityIndexIntegrationTest.java
@@ -15,7 +15,6 @@ import junit.framework.Assert;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
