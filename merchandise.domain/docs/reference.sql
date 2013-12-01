dd645bc5ab00340c88ff71dcaf4df14588002747
Manfred Quasten <manfred.quasten@arcor.de>
2013-12-01 15:40:56 +0100

    postgres back to its old port, script to create references

---
+++ merchandise.domain/docs/reference.sql
@@ -0,0 +1,5 @@
+insert into reference(key, referencetype) values('de', 'Language'); 
+insert into reference(key, referencetype) values('en', 'Language'); 
+insert into reference(key, referencetype) values('de', 'Country'); 
+insert into reference(key, referencetype) values('us', 'Country'); 
+insert into reference(key, referencetype) values('gb', 'Country'); 
