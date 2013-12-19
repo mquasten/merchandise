c6306afdfbc20a3f30a3a1c500a39ae7fa8f3ee7
Manfred Quasten <manfred.quasten@arcor.de>
2013-12-19 19:04:09 +0100

    cosmetics

--- merchandise.application/merchandise.application/src/test/java/de/mq/merchandise/opportunity/support/DocumentRepositoryIntegrationTest.java
+++ merchandise.application/merchandise.application/src/test/java/de/mq/merchandise/opportunity/support/DocumentRepositoryIntegrationTest.java
@@ -7,12 +7,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Map;
 
-
-
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
