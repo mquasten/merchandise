8e4bbf337d32b0b1699c52037ee5ef7d03d46de9
Manfred Quasten <manfred.quasten@arcor.de>
2014-06-11 22:25:50 +0200

    prices for pets

--- merchandise.application/merchandise.application/src/main/groovy/de/mq/merchandise/rule/support/SimpleOrderItemRestClient.groovy
+++ merchandise.application/merchandise.application/src/main/groovy/de/mq/merchandise/rule/support/SimpleOrderItemRestClient.groovy
@@ -1,8 +1,13 @@
 package de.mq.merchandise.rule.support
 
  
+import org.codehaus.jackson.map.ObjectMapper;
+import org.springframework.dao.DataAccessException
+import org.springframework.dao.InvalidDataAccessApiUsageException
 import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.util.CollectionUtils;
+import org.springframework.web.client.RestOperations;
+import org.springframework.web.client.RestTemplate;
 
 
 import groovy.lang.MetaClass;
@@ -13,22 +18,56 @@ import groovyx.net.http.RESTClient;
 
 class SimpleOrderItemRestClient implements  de.mq.merchandise.rule.Converter<OrderItem, Map<String,Object>>  {
 
-	def RESTClient restClient = new RESTClient('http://localhost:5984/petstore/_design/qualityByArtist/_view/QualityByArtist',   groovyx.net.http.ContentType.JSON);
+	def RESTClient rest = new RESTClient('http://localhost:5984/petstore/_design/',   groovyx.net.http.ContentType.JSON);
 	
 	@Override
 	public Map<String,Object> convert(final OrderItem source) {
 	
-		final Map<String,String> map = new HashMap<>();
-		map.put("key" , String.format("\"%s\"" , "nicole"));
+		final Map<String,String> queryMap = new HashMap<>();
+		queryMap.put("key" , String.format("\"%s\"" , "nicole"));
+		
 	
-	    final HttpResponseDecorator results  = restClient.get( 'query' :  map );
+		
+	    final HttpResponseDecorator results  = rest.get(  'path' :'qualityByArtist/_view/QualityByArtist', query : queryMap );
 	
-
 		final  String quality = DataAccessUtils.requiredSingleResult(results.responseData.get("rows")).get("value");
+		if( quality == null){
+			throw new InvalidDataAccessApiUsageException("Result is invalid");
+		}
 		
-		println quality
 		
-		return null;
+		queryMap.clear();
+		
+		queryMap.put("key" , String.format("{\"quality\":\"%s\",\"unit\":\"date\"  }", quality));
+		
+		final HttpResponseDecorator prices  = rest.get(  'path' :'pricePerUnit/_view/PricePerUnit', query : queryMap );
+		
+	    int quantity = 4;
+		if( CollectionUtils.isEmpty(prices.responseData.get("rows")) ){
+			throw new InvalidDataAccessApiUsageException("Result is invalid");
+		}
+	
+		for( final Map<String,Object> price : prices.responseData.get("rows")) {
+			
+			if( CollectionUtils.isEmpty(price.value)){
+				throw new InvalidDataAccessApiUsageException("Result is invalid");
+			}
+			Integer min  =  price.value.min ;
+			if( min == null){
+				min = 0 ;  
+			}
+			Integer max = price.value.max ;
+			if( max == null) {
+				max=Integer.MAX_VALUE;
+			}
+			
+			if( (quantity >=  min.intValue()) && (quantity <=  max.intValue()) ) {
+				println "***" + price.value.pricePerUnit + "***"
+				return;
+			}
+		}
+		throw new InvalidDataAccessApiUsageException("PricePerUnit not found for Parameters");
+		
 	   
 	}
 
