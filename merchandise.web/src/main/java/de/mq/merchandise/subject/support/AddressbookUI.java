d1bb7f5e65e75284d431cc890fdadc3ca951e38a
Manfred Quasten <manfred.quasten@arcor.de>
2015-04-24 21:16:50 +0200

    LazyQuery-Container

--- merchandise.web/src/main/java/de/mq/merchandise/subject/support/AddressbookUI.java
+++ merchandise.web/src/main/java/de/mq/merchandise/subject/support/AddressbookUI.java
@@ -1,26 +1,21 @@
 package de.mq.merchandise.subject.support;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.vaadin.addons.lazyquerycontainer.LazyQueryContainer;
 import org.vaadin.addons.lazyquerycontainer.Query;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Title;
-import com.vaadin.data.Container.Filter;
 import com.vaadin.data.Item;
-import com.vaadin.data.Property;
-import com.vaadin.data.Property.ValueChangeEvent;
 import com.vaadin.data.fieldgroup.FieldGroup;
-import com.vaadin.data.util.BeanItem;
-import com.vaadin.data.util.IndexedContainer;
 import com.vaadin.data.util.ObjectProperty;
 import com.vaadin.data.util.PropertysetItem;
-import com.vaadin.event.FieldEvents.TextChangeEvent;
-import com.vaadin.event.FieldEvents.TextChangeListener;
 import com.vaadin.ui.AbstractTextField.TextChangeEventMode;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
@@ -33,7 +28,6 @@ import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
 
 import de.mq.merchandise.customer.CustomerService;
-import de.mq.merchandise.subject.Subject;
 
 
 /* 
@@ -50,48 +44,36 @@ public class AddressbookUI extends AbstractUIBeanInjector {
 
 	private static final long serialVersionUID = 1L;
 	
+	
+	
 	@Autowired
 	private CustomerService customerService;
 	
 	
 	
 	
+	enum SubjectCols {
+		
+		Id(false, Long.class, -1L),
+		Name(true, String.class, ""),
+		Description(true, String.class, ""),
+		DateCreated(true, Date.class, null);
+		
+		private final boolean visible ;
+		
+		SubjectCols(boolean visible, Class<?> type, Object defaultValue) {
+			this.visible=visible;
+		}
+		boolean visible() {
+			return visible;
+		}
+		
+		
+		
+		
+	}
 	
-	final LazyQueryContainer container=new LazyQueryContainer(queryDefinition -> new Query() {
-
-		@Override
-		public Item constructItem() {
-			// TODO Auto-generated method stub
-			Item i = new PropertysetItem();
-			i.addItemProperty("id",new ObjectProperty<>("19680528") );
-			return new BeanItem<Subject>(BeanUtils.instantiateClass(SubjectImpl.class));
-		}
-
-		@Override
-		public boolean deleteAllItems() {
-			throw new UnsupportedOperationException();
-			
-		}
-
-		@Override
-		public List<Item> loadItems(final int startIndex, int count) {
-			// Backend
-			return null;
-		}
-
-		@Override
-		public void saveItems(final List<Item> added, final List<Item> modified, final List<Item> removed) {
-				throw new UnsupportedOperationException();
-			
-		}
-
-		@Override
-		public int size() {
-			// Backend
-			return 0;
-		}} ,null, 50, false);
-
-
+	 
 	/* User interface components are stored in session. */
 	private Table contactList = new Table();
 	private TextField searchField = new TextField();
@@ -112,7 +94,7 @@ public class AddressbookUI extends AbstractUIBeanInjector {
 	 * just a dummy in-memory list, but there are many more practical
 	 * implementations.
 	 */
-	IndexedContainer contactContainer ;
+	
 
 	
 
@@ -189,101 +171,18 @@ public class AddressbookUI extends AbstractUIBeanInjector {
 
 	private void initSearch() {
 
-		/*
-		 * We want to show a subtle prompt in the search field. We could also
-		 * set a caption that would be shown above the field or description to
-		 * be shown in a tooltip.
-		 */
+		
 		searchField.setInputPrompt("Search contacts");
 
-		/*
-		 * Granularity for sending events over the wire can be controlled. By
-		 * default simple changes like writing a text in TextField are sent to
-		 * server with the next Ajax call. You can set your component to be
-		 * immediate to send the changes to server immediately after focus
-		 * leaves the field. Here we choose to send the text over the wire as
-		 * soon as user stops writing for a moment.
-		 */
+		
 		searchField.setTextChangeEventMode(TextChangeEventMode.LAZY);
 
-		/*
-		 * When the event happens, we handle it in the anonymous inner class.
-		 * You may choose to use separate controllers (in MVC) or presenters (in
-		 * MVP) instead. In the end, the preferred application architecture is
-		 * up to you.
-		 */
-		searchField.addTextChangeListener(new TextChangeListener() {
-			
-			private static final long serialVersionUID = 1L;
+		
 
-			public void textChange(final TextChangeEvent event) {
 
-				/* Reset the filter for the contactContainer. */
-				contactContainer.removeAllContainerFilters();
-				contactContainer.addContainerFilter(new ContactFilter(event
-						.getText()));
-			}
-		});
-	}
+	
 
-	/*
-	 * A custom filter for searching names and companies in the
-	 * contactContainer.
-	 */
-	private class ContactFilter implements Filter {
-		/**
-		 * 
-		 */
-		private static final long serialVersionUID = 1L;
-		private String needle;
-
-		public ContactFilter(String needle) {
-			this.needle = needle.toLowerCase();
-		}
-
-		public boolean passesFilter(Object itemId, Item item) {
-			String haystack = ("" + item.getItemProperty(FNAME).getValue()
-					+ item.getItemProperty(LNAME).getValue() + item
-					.getItemProperty(COMPANY).getValue()).toLowerCase();
-			return haystack.contains(needle);
-		}
-
-		public boolean appliesToProperty(Object id) {
-			return true;
-		}
-	}
-
-	private void initAddRemoveButtons() {
-		addNewContactButton.addClickListener(new ClickListener() {
-			/**
-			 * 
-			 */
-			private static final long serialVersionUID = 1L;
-
-			@SuppressWarnings("unchecked")
-			public void buttonClick(ClickEvent event) {
-
-				/*
-				 * Rows in the Container data model are called Item. Here we add
-				 * a new row in the beginning of the list.
-				 */
-				contactContainer.removeAllContainerFilters();
-				Object contactId = contactContainer.addItemAt(0);
-
-				/*
-				 * Each Item has a set of Properties that hold values. Here we
-				 * set a couple of those.
-				 */
-				contactList.getContainerProperty(contactId, FNAME).setValue(
-						"New");
-				contactList.getContainerProperty(contactId, LNAME).setValue(
-						"Contact");
-
-				/* Lets choose the newly created contact to edit it. */
-				contactList.select(contactId);
-			}
-		});
-
+	
 		removeContactButton.addClickListener(new ClickListener() {
 		
 			private static final long serialVersionUID = 1L;
@@ -296,81 +195,93 @@ public class AddressbookUI extends AbstractUIBeanInjector {
 	}
 
 	private void initContactList() {
-		contactList.setContainerDataSource(contactContainer);
-		contactList.setVisibleColumns(new Object[] { FNAME, LNAME, "Street" , "City", "Distance" });
-		contactList.setSelectable(true);
-		contactList.setImmediate(true);
-		
-
-		contactList.addValueChangeListener(new Property.ValueChangeListener() {
-			
-			private static final long serialVersionUID = 1L;
-
-			public void valueChange(ValueChangeEvent event) {
-				Object contactId = contactList.getValue();
-
-				/*
-				 * When a contact is selected from the list, we want to show
-				 * that in our editor on the right. This is nicely done by the
-				 * FieldGroup that binds all the fields to the corresponding
-				 * Properties in our contact at once.
-				 */
-				if (contactId != null){
-					editorFields.setItemDataSource(contactList
-							.getItem(contactId));
-			   
-				}
-				editorLayout.setVisible(contactId != null);
-				
-			
-			}
-		});
-	}
-
-	/*
-	 * Generate some in-memory example data to play with. In a real application
-	 * we could be using SQLContainer, JPAContainer or some other to persist the
-	 * data.
-	 */
-	@SuppressWarnings("unchecked")
-	private  IndexedContainer createDummyDatasource() {
-		IndexedContainer ic = new IndexedContainer();
-
-		for (String p : fieldNames) {
-			ic.addContainerProperty(p, String.class, "");
-		}
-
-		/* Create dummy data by randomly combining first and last names */
-		String[] fnames = { "Peter", "Alice", "Joshua", "Mike", "Olivia",
-				"Nina", "Alex", "Rita", "Dan", "Umberto", "Henrik", "Rene",
-				"Lisa", "Marge" };
-		String[] lnames = { "Smith", "Gordon", "Simpson", "Brown", "Clavel",
-				"Simons", "Verne", "Scott", "Allison", "Gates", "Rowling",
-				"Barks", "Ross", "Schneider", "Tate" };
-		Object referenceId = ic.addItem();
 	
-		ic.getContainerProperty(referenceId, FNAME).setValue("Eigene Adresse");
-		ic.getContainerProperty(referenceId, LNAME).setValue("Home-Location");
-		ic.getContainerProperty(referenceId, "Street").setValue("Bahnhofstraße 123");
-		ic.getContainerProperty(referenceId, "City").setValue("Wegberg");
-		ic.getContainerProperty(referenceId, "Zip").setValue("41844");
-		ic.getContainerProperty(referenceId, "Country").setValue("DE");
-		ic.getContainerProperty(referenceId, "State").setValue("NRW");
+		
+		final LazyQueryContainer container = newContainer();
+	
 		
 		
 		
 		
 		
-		for (int i = 0; i < 1000; i++) {
-			final Object id = ic.addItem();
-			ic.getContainerProperty(id, FNAME).setValue(
-					fnames[(int) (fnames.length * Math.random())]);
-			ic.getContainerProperty(id, LNAME).setValue(
-					lnames[(int) (lnames.length * Math.random())]);
-		}
+		contactList.setContainerDataSource(container);
+		
+		contactList.setVisibleColumns(Arrays.asList(SubjectCols.values()).stream().filter(col -> col.visible() ).collect(Collectors.toList()).toArray());
+	
+		contactList.setSelectable(true);
+		
+		contactList.setBuffered(true);
+		contactList.setSortContainerPropertyId(SubjectCols.Name);
 
-		return ic;
 	}
+
+	private LazyQueryContainer newContainer() {
+		
+		final LazyQueryContainer result =  new LazyQueryContainer(queryDefinition -> new Query() {
+		
+				
+				@Override
+				public Item constructItem() {
+				    // Diese Methode ist Sinnlos!!!
+					return null;
+				}
+		
+				@Override
+				public boolean deleteAllItems() {
+					throw new UnsupportedOperationException();
+					
+				}
+		
+				@Override
+				public List<Item> loadItems(final int startIndex, int count) {
+					final Item item1 = new PropertysetItem();
+				
+					System.out.println("loadItems");
+					System.out.println(contactList.getSortContainerPropertyId() + ":"+  contactList.isSortAscending());
+					
+					item1.addItemProperty(SubjectCols.Id, new  ObjectProperty<>(19680528L));
+					item1.addItemProperty(SubjectCols.Name, new  ObjectProperty<>("Pets4You"));
+					item1.addItemProperty(SubjectCols.Description, new  ObjectProperty<>("Pussycat Escort-Service"));
+					
+					item1.addItemProperty(SubjectCols.DateCreated, new  ObjectProperty<>(new Date()));
+					
+					final Item item2 = new PropertysetItem();
+				
+				
+					
+					item2.addItemProperty(SubjectCols.Id, new  ObjectProperty<>(4711));
+					item2.addItemProperty(SubjectCols.Name, new  ObjectProperty<>("Crime Bank"));
+					item2.addItemProperty(SubjectCols.Description, new  ObjectProperty<>("Don Colerone's Banking Service"));
+					
+					item2.addItemProperty(SubjectCols.DateCreated, new  ObjectProperty<>(new Date()));
+					
+					
+					final List<Item>  results = new ArrayList<>();
+					results.add(item1);
+					results.add(item2);
+					return results;
+				}
+		
+				@Override
+				public void saveItems(final List<Item> added, final List<Item> modified, final List<Item> removed) {
+						throw new UnsupportedOperationException();
+					
+				}
+		
+				@Override
+				public int size() {
+					
+					System.out.println("size");
+					// Backend
+					return 2;
+				}} ,SubjectCols.Id, 50, true);
+		
+		     Arrays.asList(SubjectCols.values()).forEach(col -> result.addContainerProperty(col, String.class , "", true,true)); 
+		
+			
+		return result; 
+	}
+	
 
 	
 
@@ -380,12 +291,12 @@ public class AddressbookUI extends AbstractUIBeanInjector {
 		System.out.println("********************");
 		System.out.println(customerService);
 		System.out.println("********************");
-		contactContainer = createDummyDatasource();
+	
 		initLayout();
 		initContactList();
 		initEditor();
 		initSearch();
-		initAddRemoveButtons();
+		
 		
 	}
 	
