cd7b557c9580309f9a180f8354e49d31e898c3d9
Manfred Quasten <manfred.quasten@arcor.de>
2015-05-24 17:04:11 +0200

    Eventhandling with Spring 4.2

--- merchandise.web/src/main/java/de/mq/merchandise/util/support/SimpleEventListenerImpl.java
+++ merchandise.web/src/main/java/de/mq/merchandise/util/support/SimpleEventListenerImpl.java
@@ -9,7 +9,6 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.event.EventListener;
@@ -19,41 +18,37 @@ import org.springframework.util.ReflectionUtils;
 import de.mq.merchandise.subject.support.SubjectEventQualifier;
 import de.mq.merchandise.util.Event;
 
-
 @Controller()
-class SimpleEventListenerImpl  implements ApplicationContextAware{
-	
-	final Map<Object,Method> methods = new HashMap<>();
-	
-	private Set<Class<? extends Annotation>> qualifiers = new HashSet<>() ; 
-	
+class SimpleEventListenerImpl implements ApplicationContextAware {
+
+	private static final String ANNOTATION_DEFAULT_VALUE_METHOD = "value";
+	private final Map<Object, Method> methods = new HashMap<>();
+	private final Map<Object, Object> targets = new HashMap<>();
+
+	private Set<Class<? extends Annotation>> qualifiers = new HashSet<>();
+
 	SimpleEventListenerImpl() {
 		qualifiers.add(SubjectEventQualifier.class);
 	}
-	
+
 	@EventListener
 	<T> void test(final Event<T> event) {
 		System.out.println(methods);
 		System.out.println("****");
-		System.out.println(event.id() );
-		//event.assign(  "hottest artist ever");
-		
+		System.out.println(targets);
+		System.out.println(event.id());
+		// event.assign( "hottest artist ever");
+
 	}
 
 	@Override
-	public void setApplicationContext(final ApplicationContext applicationContext) throws BeansException {
+	public void setApplicationContext(final ApplicationContext applicationContext)  {
 		final Set<Object> controllers = applicationContext.getBeansWithAnnotation(Controller.class).values().stream().collect(Collectors.toSet());
-	    controllers.forEach(obj -> Arrays.asList(obj.getClass().getDeclaredMethods()).stream().forEach(m -> Arrays.asList(m.getAnnotations()).stream().filter( a ->  qualifiers.contains(a.annotationType()) && ReflectionUtils.findMethod(a.annotationType(), "value") != null && ReflectionUtils.invokeMethod(ReflectionUtils.findMethod(a.annotationType(), "value"), a) != null).forEach(a ->methods.put( ReflectionUtils.invokeMethod(ReflectionUtils.findMethod(a.annotationType(), "value"), a) , m))));
+		controllers.forEach(obj -> Arrays.asList(obj.getClass().getDeclaredMethods()).stream().forEach(m -> Arrays.asList(m.getAnnotations()).stream().filter(a -> qualifiers.contains(a.annotationType()) && ReflectionUtils.findMethod(a.annotationType(), ANNOTATION_DEFAULT_VALUE_METHOD) != null && ReflectionUtils.invokeMethod(ReflectionUtils.findMethod(a.annotationType(), ANNOTATION_DEFAULT_VALUE_METHOD), a) != null).forEach(a -> {
+			final Object event = ReflectionUtils.invokeMethod(ReflectionUtils.findMethod(a.annotationType(), ANNOTATION_DEFAULT_VALUE_METHOD), a);
+			methods.put(event, m);
+			targets.put(event, obj);
+		})));
 	}
-
-	
-
-	
-
-	
-
-	
-
-	
 
 }
