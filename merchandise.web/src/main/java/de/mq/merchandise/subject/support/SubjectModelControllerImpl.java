5e5a15352fa7a185dcee8021383519d1180190df
Manfred Quasten <manfred.quasten@arcor.de>
2015-07-10 20:15:42 +0200

    coverage

--- merchandise.web/src/main/java/de/mq/merchandise/subject/support/SubjectModelControllerImpl.java
+++ merchandise.web/src/main/java/de/mq/merchandise/subject/support/SubjectModelControllerImpl.java
@@ -16,102 +16,92 @@ import de.mq.merchandise.support.Mapper;
 
 @Controller
 class SubjectModelControllerImpl {
-	
-	
-	private final  SubjectService subjectService;
-	private final  Mapper<Subject,Subject> subjectIntoSubjectMapper;
-	
-	private final  Mapper<Condition,Subject> conditionIntoSubjectMapper;
-	
-	
+
+	private final SubjectService subjectService;
+	private final Mapper<Subject, Subject> subjectIntoSubjectMapper;
+
+	private final Mapper<Condition, Subject> conditionIntoSubjectMapper;
+
 	@Autowired
-	SubjectModelControllerImpl(final SubjectService subjectService, final @SubjectMapper(SubjectMapperType.Subject2Subject) Mapper<Subject,Subject> subjectIntoSubjectMapper, final @SubjectMapper(SubjectMapperType.Condition2Subject) Mapper<Condition,Subject>conditionIntoSubjectMapper) {
-		this.subjectService=subjectService;
-		this.subjectIntoSubjectMapper=subjectIntoSubjectMapper;
-		this.conditionIntoSubjectMapper=conditionIntoSubjectMapper;
+	SubjectModelControllerImpl(final SubjectService subjectService, final @SubjectMapper(SubjectMapperType.Subject2Subject) Mapper<Subject, Subject> subjectIntoSubjectMapper, final @SubjectMapper(SubjectMapperType.Condition2Subject) Mapper<Condition, Subject> conditionIntoSubjectMapper) {
+		this.subjectService = subjectService;
+		this.subjectIntoSubjectMapper = subjectIntoSubjectMapper;
+		this.conditionIntoSubjectMapper = conditionIntoSubjectMapper;
 	}
-	
-	
-	
+
 	@SubjectEventQualifier(EventType.CountPaging)
 	Number countSubjects(final SubjectModel subjectModel) {
 		return subjectService.subjects(subjectModel.getSearchCriteria());
-	
-	}
-	
 
+	}
 
 	@SubjectEventQualifier(EventType.ListPaging)
 	Collection<Subject> subjects(final SubjectModel subjectModel, final ResultNavigation paging) {
 		return subjectService.subjects(subjectModel.getSearchCriteria(), paging);
 	}
-	
+
 	@SubjectEventQualifier(EventType.SubjectChanged)
-	Subject subject(final Long id ) {
+	Subject subject(final Long id) {
 		return subjectService.subject(id);
 	}
-	
+
 	@SubjectEventQualifier(EventType.ConditionChanged)
-	Condition condition(final SubjectModel subjectModel , final Long id ) {
+	Condition condition(final SubjectModel subjectModel, final Long id) {
 		Assert.notNull(id, "Id is mandatory");
 		Assert.isTrue((subjectModel.getSubject().isPresent()), "Subject is mandatory");
 		Assert.isTrue((subjectModel.getSubject().get().id().isPresent()), "SubjectId is mandatory");
-		final Subject subject =  subjectService.subject(subjectModel.getSubject().get().id().get());
-		
+		final Subject subject = subjectService.subject(subjectModel.getSubject().get().id().get());
+
 		final Optional<Condition> result = subject.conditions().stream().filter(condition -> condition.id().orElse(-1L).equals(id)).findFirst();
 		Assert.isTrue(result.isPresent(), "Condition should be persistent");
 		return result.get();
 	}
-	
+
 	@SubjectEventQualifier(EventType.SubjectSaved)
-	void save(final Long subjectId, final Subject subject ) {
+	void save(final Long subjectId, final Subject subject) {
 		Assert.notNull(subject.customer(), "Customer is mandatory");
-		if( subjectId == null){
+		if (subjectId == null) {
 			subjectService.save(new SubjectImpl(subject.customer(), subject.name(), subject.description()));
 			return;
 		}
-		
-		 final Subject toBeChanged = subjectService.subject(subjectId);
-		 Assert.notNull(toBeChanged.customer(), "Customer is mandatory");
-		
-		 Assert.isTrue(toBeChanged.customer().equals(subject.customer()));
-		
-		 subjectIntoSubjectMapper.mapInto(subject, toBeChanged);
-		 
-		 subjectService.save(toBeChanged);
-		
+
+		final Subject toBeChanged = subjectService.subject(subjectId);
+		Assert.notNull(toBeChanged.customer(), "Customer is mandatory");
+
+		Assert.isTrue(toBeChanged.customer().equals(subject.customer()));
+
+		subjectIntoSubjectMapper.mapInto(subject, toBeChanged);
+
+		subjectService.save(toBeChanged);
+
 	}
-	
+
 	@SubjectEventQualifier(EventType.ConditionSaved)
-	
 	Subject save(final Condition condition, final Long subjectId) {
 		Assert.notNull(subjectId, "Subject should be persistent");
-		final Subject subject =subjectService.subject(subjectId);
+		final Subject subject = subjectService.subject(subjectId);
 		conditionIntoSubjectMapper.mapInto(condition, subject);
 		subjectService.save(subject);
 		return subject;
 	}
-	
+
 	@SubjectEventQualifier(EventType.SubjectDeleted)
 	void delete(final Subject subject) {
-		
+
 		subjectService.remove(subject);
-		
+
 	}
-	
+
 	@SubjectEventQualifier(EventType.ConditionDeleted)
 	Subject delete(final Condition condition, final Long subjectId) {
 		Assert.notNull(subjectId, "Subject should be persistent");
 		Assert.notNull(condition.conditionType(), "ConditionType is mandatory");
 		final Subject subject = subjectService.subject(subjectId);
-		System.out.println(subject.conditionTypes());
+
 		subject.remove(condition.conditionType());
-		
-		System.out.println(condition.conditionType());
+
 		subjectService.save(subject);
 		return subject;
 	}
 
-	
-	
 }
--- merchandise.web/src/test/java/de/mq/merchandise/subject/support/SubjectModelControllerTest.java
+++ merchandise.web/src/test/java/de/mq/merchandise/subject/support/SubjectModelControllerTest.java
@@ -6,18 +6,31 @@ package de.mq.merchandise.subject.support;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Optional;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 
 import de.mq.merchandise.ResultNavigation;
+import de.mq.merchandise.customer.Customer;
 import de.mq.merchandise.subject.Condition;
 import de.mq.merchandise.subject.Subject;
 import de.mq.merchandise.support.Mapper;
 
 public class SubjectModelControllerTest {
 	
+	private static final String CONDITION_QUALITY = "Quality";
+
+	private static final String DESC = "PetsForYou";
+
+	private static final String NAME = "PetStore";
+
+	private static final long CONDITION_ID = 4711L;
+
+	private static final long ID = 19680228L;
+
 	private static final Integer ROW_COUNTER = Integer.valueOf(42);
 	
 	private ResultNavigation paging = Mockito.mock(ResultNavigation.class);
@@ -50,6 +63,86 @@ public class SubjectModelControllerTest {
 		Mockito.when(subjectService.subjects(subject, paging)).thenReturn(results);
 		Assert.assertEquals(results, subjectModelController.subjects(model, paging));
 		
+	}
+	
+	@Test
+	public final void subject() {
+		Mockito.when(subjectService.subject(ID)).thenReturn(subject);
+		
+		Assert.assertEquals(subject, subjectModelController.subject(ID));
+	}
+	
+	@Test
+	public final void  condition() {
+		Mockito.when(subject.id()).thenReturn(Optional.of(ID));
+		Mockito.when(model.getSubject()).thenReturn(Optional.of(subject));
+		Mockito.when(subjectService.subject(ID)).thenReturn(subject);
+		final Collection<Condition> conditions = new ArrayList<>();
+		final Condition condition = Mockito.mock(Condition.class);
+		conditions.add(condition);
+		Mockito.when(condition.id()).thenReturn(Optional.of(CONDITION_ID));
+		Mockito.when(subject.conditions()).thenReturn(conditions);
+		
+		Assert.assertEquals(condition, subjectModelController.condition(model, CONDITION_ID));
+	}
+	
+	@Test
+	public final void save() {
+		final Customer customer = Mockito.mock(Customer.class);
+		Mockito.when(subject.customer()).thenReturn(customer);
+		final Subject existing = Mockito.mock(Subject.class);
+		Mockito.when(existing.customer()).thenReturn(customer);
+		Mockito.when(subjectService.subject(ID)).thenReturn(existing);
+		subjectModelController.save(ID, subject);
+		
+		Mockito.verify(subjectIntoSubjectMapper).mapInto(subject, existing);
+		Mockito.verify(subjectService).save(existing);
+	}
+	
+	@Test
+	public final void saveNew() {
+		final Customer customer = Mockito.mock(Customer.class);
+		Mockito.when(subject.customer()).thenReturn(customer);
+		Mockito.when(subject.name()).thenReturn(NAME);
+		Mockito.when(subject.description()).thenReturn(DESC);
+		
+		subjectModelController.save(null, subject);
+		final ArgumentCaptor<Subject> subjectCaptor = ArgumentCaptor.forClass(Subject.class);
+		Mockito.verify(subjectService).save(subjectCaptor.capture());
+		
+		
+		Assert.assertTrue(subjectCaptor.getValue() instanceof SubjectImpl);
+		Assert.assertEquals(NAME, subjectCaptor.getValue().name());
+		Assert.assertEquals(DESC, subjectCaptor.getValue().description());
+	}
+	
+	@Test
+	public final void  saveCondition() {
+		final Condition condition = Mockito.mock(Condition.class);
+		Mockito.when(subjectService.subject(ID)).thenReturn(subject);
+		
+		Assert.assertEquals(subject, subjectModelController.save(condition, ID));
+		
+		Mockito.verify(conditionIntoSubjectMapper).mapInto(condition, subject);
+		Mockito.verify(subjectService).save(subject);
+	}
+	
+	@Test
+	public final void  delete() {
+		
+		subjectModelController.delete(subject);
+		Mockito.verify(subjectService).remove(subject);
+	}
+	
+	@Test
+	public final void  deleteCondition() {
+		final Condition condition = Mockito.mock(Condition.class);
+		Mockito.when(condition.conditionType()).thenReturn(CONDITION_QUALITY);
+		
+		Mockito.when(subjectService.subject(ID)).thenReturn(subject);
+		Assert.assertEquals(subject, subjectModelController.delete(condition, ID));
+		Mockito.verify(subject).remove(CONDITION_QUALITY);
+		Mockito.verify(subjectService).save(subject);
 	}
 
 }
