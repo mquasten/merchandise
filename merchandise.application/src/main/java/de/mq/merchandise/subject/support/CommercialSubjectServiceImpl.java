40df4ed04979a864d31c1d5227cc112c3ecb8d60
Manfred Quasten <manfred.quasten@arcor.de>
2015-07-27 21:19:15 +0200

    Query CommercialSubjects

---
+++ merchandise.application/src/main/java/de/mq/merchandise/subject/support/CommercialSubjectServiceImpl.java
@@ -0,0 +1,16 @@
+package de.mq.merchandise.subject.support;
+
+import java.util.Collection;
+
+public class CommercialSubjectServiceImpl {
+	
+	public final Collection<CommercialSubjet>commercialSubjects(CommercialSubjet commercialSubject  ) {
+		
+		
+		
+		
+		return null;
+		
+	}
+
+}
