fc84ba892f94b91963c566bb24277e8df9ef3b14
Manfred Quasten <manfred.quasten@arcor.de>
2015-12-15 21:00:06 +0100

    Coverage

--- merchandise.web/src/main/java/de/mq/merchandise/subject/support/ItemToCommercialSubjectConverterImpl.java
+++ merchandise.web/src/main/java/de/mq/merchandise/subject/support/ItemToCommercialSubjectConverterImpl.java
@@ -12,49 +12,46 @@ import de.mq.merchandise.support.ReflectionBasedFieldMapperImpl;
 
 @Component
 @CommercialSubjectModelQualifier(CommercialSubjectModelQualifier.Type.ItemToCommercialSubjectConverter)
-public class ItemToCommercialSubjectConverterImpl extends   ReflectionBasedFieldMapperImpl implements Converter<Item,CommercialSubject>{
+public class ItemToCommercialSubjectConverterImpl extends ReflectionBasedFieldMapperImpl implements Converter<Item, CommercialSubject> {
 
-	private static final String ITEM_NAME_FIELD = "name";
-	private static final String SUBJECT_ID_FIELD = "id";
-	private static final String SUBJECT_DESC_FIELD = "description";
-	private static final String SUBJECT_NAME_FIELD = ITEM_NAME_FIELD;
-	private static final String COMMERCIAL_SUBJECT_NAME_FIELD = SUBJECT_NAME_FIELD;
-	private static final String COMMERCIAL_SUBJECT_ID_FIELD = SUBJECT_ID_FIELD;
+	static final String ITEM_NAME_FIELD = "name";
+	static final String SUBJECT_ID_FIELD = "id";
+	static final String SUBJECT_DESC_FIELD = "description";
+	static final String SUBJECT_NAME_FIELD = ITEM_NAME_FIELD;
+	static final String COMMERCIAL_SUBJECT_NAME_FIELD = SUBJECT_NAME_FIELD;
+	static final String COMMERCIAL_SUBJECT_ID_FIELD = SUBJECT_ID_FIELD;
 
 	@Override
 	public CommercialSubject convert(final Item source) {
 
 		final CommercialSubject target = BeanUtils.instantiateClass(CommercialSubjectImpl.class);
-		
+
 		final Subject subject = BeanUtils.instantiateClass(SubjectImpl.class);
-		
+
 		assign(SUBJECT_NAME_FIELD, subject, value(source, CommercialSubjectCols.SubjectName));
-		assign(SUBJECT_DESC_FIELD, subject, value(source,CommercialSubjectCols.SubjectDesc));
-		assign(SUBJECT_ID_FIELD, subject , -1L);
-		
+		assign(SUBJECT_DESC_FIELD, subject, value(source, CommercialSubjectCols.SubjectDesc));
+		assign(SUBJECT_ID_FIELD, subject, -1L);
+
 		target.assign(subject, ITEM_NAME_FIELD, false);
-		
+
 		final CommercialSubjectItem item = target.commercialSubjectItem(subject).get();
-		assign(ITEM_NAME_FIELD , item, value(source,CommercialSubjectCols.ItemName) );
-		
-		assign(SUBJECT_ID_FIELD, subject , null);
-		
+		assign(ITEM_NAME_FIELD, item, value(source, CommercialSubjectCols.ItemName));
+
+		assign(SUBJECT_ID_FIELD, subject, null);
+
 		assign(COMMERCIAL_SUBJECT_ID_FIELD, target, value(source, CommercialSubjectCols.Id));
 		assign(COMMERCIAL_SUBJECT_NAME_FIELD, target, value(source, CommercialSubjectCols.Name));
-		
+
 		return target;
 	}
 
-	private <T> T value(final Item source, Object id ) {
+	private <T> T value(final Item source, Object id) {
 		@SuppressWarnings("unchecked")
 		final Property<T> property = source.getItemProperty(id);
-		if(property==null){
+		if (property == null) {
 			return null;
 		}
 		return property.getValue();
 	}
-	
-	
-	
 
 }
---
+++ merchandise.web/src/test/java/de/mq/merchandise/subject/support/ItemToCommercialSubjectConverter.java
@@ -0,0 +1,80 @@
+package de.mq.merchandise.subject.support;
+
+import java.util.Arrays;
+
+
+
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.core.convert.converter.Converter;
+
+import com.vaadin.data.Item;
+
+import com.vaadin.data.util.ObjectProperty;
+import com.vaadin.data.util.PropertysetItem;
+
+public class ItemToCommercialSubjectConverter {
+
+	private static final Long ID = 19680528L;
+
+	private final Converter<Item,CommercialSubject> converter = new ItemToCommercialSubjectConverterImpl();
+	
+	final Item item = new PropertysetItem();
+	
+	@Before
+	public final void setup() {
+		
+		Arrays.asList(CommercialSubjectCols.values()).stream().forEach(col -> {
+			if(col.target().equals(String.class)) {
+				item.addItemProperty(col, new ObjectProperty<>(col.name().toLowerCase()));
+			} else if(col.target().equals(Long.class) ) {
+				item.addItemProperty(col, new ObjectProperty<>(ID));
+			}
+			
+			
+		} );
+	
+	}
+	
+	@Test
+	public final void convert() {
+	
+		
+		final CommercialSubject result = converter.convert(item);
+		Assert.assertEquals(CommercialSubjectCols.Name.name().toLowerCase(), result.name());
+		Assert.assertEquals(1, result.commercialSubjectItems().size());
+		Assert.assertEquals(CommercialSubjectCols.ItemName.name().toLowerCase(), result.commercialSubjectItems().stream().findFirst().get().name());
+		
+		Assert.assertEquals(CommercialSubjectCols.SubjectName.name().toLowerCase(), result.commercialSubjectItems().stream().findFirst().get().subject().name());
+		Assert.assertEquals(CommercialSubjectCols.SubjectDesc.name().toLowerCase(), result.commercialSubjectItems().stream().findFirst().get().subject().description());
+		
+		Assert.assertTrue(result.id().isPresent());
+		Assert.assertEquals(ID, result.id().get());
+		
+		Assert.assertFalse(result.commercialSubjectItems().stream().findFirst().get().subject().id().isPresent());
+	}
+	
+	
+	@Test
+	public final void convertNulls() {
+		final Item item = Mockito.mock(Item.class);
+		final CommercialSubject result = converter.convert(item);
+		Assert.assertNull(result.name());
+		Assert.assertEquals(1, result.commercialSubjectItems().size());
+		Assert.assertNull(result.commercialSubjectItems().stream().findFirst().get().name());
+		
+		Assert.assertNull(result.commercialSubjectItems().stream().findFirst().get().subject().name());
+		Assert.assertNull(result.commercialSubjectItems().stream().findFirst().get().subject().description());
+		
+		Assert.assertFalse(result.id().isPresent());
+	
+		
+		Assert.assertFalse(result.commercialSubjectItems().stream().findFirst().get().subject().id().isPresent());
+	
+	}
+	
+	
+}
